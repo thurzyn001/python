fa4d7ac40a4ff31507d500b72e1e291478719c25
thurzyn001 <zynthur@gmail.com>
2026-07-14 18:54:48 -0300

    Refactor number comparison to use ternary operator
    Replace if-elif-else statement with a single conditional expression for more concise code. Slightly updated output messages for clarity.

--- Mundo 2/ex038.py
+++ Mundo 2/ex038.py
@@ -1,10 +1,4 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-if num1 > num2:
-    print("O primeiro número é maior.")
-elif num2 > num1:
-    print("O segundo número é maior.")
-else:
-    print("Os números são iguais.")
-    
+print("O primeiro Número é maior" if num1 > num2 else "O segundo número é maior" if num2 > num1 else "Os dois números são iguais")
